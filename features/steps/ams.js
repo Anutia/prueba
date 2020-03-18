c1280780c69737b42696f966f83b74f46150e20d
Ana <flynnana@gmail.com>
2020-03-18 18:24:50 -0300

    creé la estructura del js ungradedText2

---
+++ features/steps/ams.js
@@ -0,0 +1,20 @@
+When('Click ungruded text', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+When('Click en la parte de edicion', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+  When('Click more', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+  When('Click save as draft para guardar', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+  When('Verificar que este en la pagina de ams', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
